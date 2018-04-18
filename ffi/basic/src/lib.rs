c350fcb2786c4aa421bd301608700e0db9d5e9c2
Ehsan M.Kermani <ehsanmo1367@gmail.com>
2018-04-18 08:51:22 -0700

    clean

--- ffi/basic/src/lib.rs
+++ ffi/basic/src/lib.rs
@@ -1,16 +1,19 @@
 #![allow(dead_code)]
 
+// call C's abs
 extern "C" {
     pub fn abs(input: i32) -> i32;
 }
 
 pub fn c_abs(input: i32) -> i32 {
     unsafe {
-        println!("calling abs of {input} from C: {ret}", input=input, ret=abs(input));
-        abs(input)
+        let ret = abs(input);
+        println!("calling abs of {input} from C: {ret}", input=input, ret=ret);
+        ret
     }
 }
 
+// expose rust_abs (which calls C abs for fun!) to C
 #[no_mangle]
 pub extern "C" fn rust_abs(input: i32) -> i32 {
     let ret = c_abs(input);
--- ffi/basic/src/main.c
+++ ffi/basic/src/main.c
@@ -4,6 +4,6 @@
 extern int32_t rust_abs(int32_t input);
 
 int main(void) {
-    printf("C calculates abs of -1 from Rust %d\n", rust_abs(-1));
+    printf("C calculates abs of -1 using Rust %d\n", rust_abs(-1));
     return 0;
 }
